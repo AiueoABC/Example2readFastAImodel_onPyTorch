325ce40e26c3dc9650df5ab01fd0c483b1003424
Ueo ABC, Ai <53726452+AiueoABC@users.noreply.github.com>
2021-08-31 09:14:23 +0900

    Update FastAI2ONNX.py

--- FastAI2ONNX.py
+++ FastAI2ONNX.py
@@ -1,9 +1,5 @@
-# import cv2
-# from fastai import *
-# from fastai.vision import *
-# import fastai
-# import numpy as np
 import torch
+
 # In case you got PosixPth problem on windows, add these lines
 import pathlib
 temp = pathlib.PosixPath
@@ -31,22 +27,18 @@ def load_learner(file_path, file='export.pkl', is_fastaiv2=False, test=None, tfm
     return model, mean, std
 
 
-mnist = r'Z:\PythonScripts\ModelConversionTest'
-# mnist = r'\\chinou-disk-02\disk1\tmp\UbiMouse_DataSet\DataForViewsonicMonitor'
-# mnist1 = r'C:\\Users\\chino_82i56t9\\OneDrive\\ドキュメント\\Python Scripts\\Ubimouse_kees'
+modelpath = r'E:\PythonScripts\ModelConversionTest'
+modelfile = 'yourFastAI_model.pkl'
 
-model, mean, std = load_learner(mnist, 'OpenedClosedModel-ResNet50.pkl', is_fastaiv2=True)
-# learn = load_learner(mnist, '15MAR2021_viewsonic.pkl')
-# learn1 = load_learner(mnist1, '24_03_Click.pkl')
+model, mean, std = load_learner(modelpath, modelfile, is_fastaiv2=True)
 
 model.eval()
-# learn1.model.eval()
 
-# x = torch.randn(1, 3, 480, 640, requires_grad=False)  #.cuda()
+# This is important, check which size of image is needed for your model. Usually, it will be good to use what you used on training
+# x = torch.randn(1, 3, [height], [width], requires_grad=False)
 x = torch.randn(1, 3, 240, 320, requires_grad=False)  #.cuda()
-torch_out = torch.onnx._export(model, x, r'Z:\PythonScripts\ModelConversionTest\OpenedClosedModel-ResNet50.onnx', export_params=True)
-# x1 = torch.randn(1, 3, 480, 640, requires_grad=False)  #.cuda()
-# torch_out1 = torch.onnx._export(learn1.model, x1, r'C:\\Users\\chino_82i56t9\\OneDrive\\ドキュメント\\Python Scripts\\Ubimouse_kees\\test_learn1.onnx', export_params=True)
+torch_out = torch.onnx._export(model, x, r'E:\PythonScripts\ModelConversionTest\ONNXModel.onnx', export_params=True)
+
 with open("model_data_mean_std.json", "w") as f:
     txt_lines = f'"mean": {mean}, "std": {std}'
     f.write(txt_lines)
